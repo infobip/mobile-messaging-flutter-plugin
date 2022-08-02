bf7181fe0fdf8c9c0df69920c3b7e76bb7ba7464
Olga Koroleva <Olga.Koroleva@infobip.com>
2022-08-02 15:45:24 +0300

    fixed release.sh

--- release.sh
+++ release.sh
@@ -1,27 +1,5 @@
 #!/bin/zsh
-#export FLUTTER=/Users/mobile_ci/Developer/flutter/bin/flutter
 export LANG=en_US.UTF-8
-
-function evalNewVersion {
-  curr_major=$(sed -n "s/^version: *\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*$/\1/p" pubspec.yaml)
-  curr_minor=$(sed -n "s/^version: *\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*$/\2/p" pubspec.yaml)
-  curr_patch=$(sed -n "s/^version: *\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*$/\3/p" pubspec.yaml)
-
-  if [ "$1" = "major" ]; then
-      curr_major=$((curr_major+1))
-      curr_minor=0
-      curr_patch=0
-  fi
-  if [ "$1" = "minor" ]; then
-      curr_minor=$((curr_minor+1))
-      curr_patch=0
-  fi
-  if [ "$1" = "patch" ]; then
-      curr_patch=$((curr_patch+1))
-  fi
-
-  echo "$curr_major.$curr_minor.$curr_patch"
-}
 
 function setVersion {
   sed -i '' -e "s/^version: *\([0-9]*\)\.\([0-9]*\)\.\([0-9]*\).*$/version: $1/" pubspec.yaml
@@ -42,21 +20,12 @@ RELEASE_COMMIT_LOG=`git log $LAST_TAG..HEAD --oneline`
 # (replacing newlines with "\n")
 echo RELEASE_COMMIT_LOG="${RELEASE_COMMIT_LOG//$'\n'/\\n}" > $PROPERTIES_FILE
 
+echo "*** [DEBUG] Setting version $RELEASE_VERSION"
 
-# Bump version
-if [ "${RELEASE}" = "custom" ]; then
-    release_version=${CUSTOM_RELEASE_VERSION}
-else
-    release_version=$(evalNewVersion $RELEASE)
-fi
-
-echo "*** [DEBUG] Setting version $release_version"
-
-setVersion $release_version
-rm -f pubspec.lock
+setVersion $RELEASE_VERSION
 
 # Update changelog
-updateChangelog $release_version
+updateChangelog $RELEASE_VERSION
 
 # Remove package lock (temporary)
 rm -f pubspec.lock
