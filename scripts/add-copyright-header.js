fcd79f7f0291f4b371a1b4642bf691c0b6256880
Martin Murcko <Martin.Murcko@infobip.com>
2025-11-05 09:12:47 +0000

    Pull request #114: MM-7169 update script
    Merge in MML/infobip-mobile-messaging-flutter from MM-7169-test-headers to main
    
    Squashed commit of the following:
    
    commit c5b36a8e3725a81e0e7e5dcb90c984ca0235916a
    Merge: 72bbdd8 2442f1a
    Author: Martin Murcko <martin.murcko@infobip.com>
    Date:   Wed Nov 5 00:10:57 2025 +0100
    
        Merge branch 'MM-7169-test-headers' of https://git.ib-ci.com/scm/mml/infobip-mobile-messaging-flutter into MM-7169-test-headers
        
        # Conflicts:
        #	ios/Classes/someFIle.swift
    
    commit 72bbdd8a69f93e91c3c1ebdf62daae418ac083e7
    Author: Martin Murcko <martin.murcko@infobip.com>
    Date:   Wed Nov 5 00:10:36 2025 +0100
    
        MM-7169 test file remove
    
    commit 2442f1aa7a9cf7ec5d27312b78150c044e172c0c
    Author: infobip-ci <infobip-ci@infobip.com>
    Date:   Wed Nov 5 00:07:04 2025 +0100
    
        Add missing copyright headers Automatically generated by Jenkins CI
    
    commit 285c6f981625d3a7a28fa52dcfc37266858f08e8
    Author: Martin Murcko <martin.murcko@infobip.com>
    Date:   Wed Nov 5 00:02:30 2025 +0100
    
        MM-7169 test file
    
    commit 06a2fad6fb24400b8690253364408c8780e2268d
    Author: Martin Murcko <martin.murcko@infobip.com>
    Date:   Tue Nov 4 23:57:35 2025 +0100
    
        MM-7169 update script

--- scripts/add-copyright-header.js
+++ scripts/add-copyright-header.js
@@ -22,6 +22,17 @@ const sourceDirs = [
   'test',         // Dart tests (optional)
 ];
 
+// Patterns to ignore (matching common .gitignore entries)
+const ignoredPatterns = [
+  'node_modules',
+  'plugins',
+  'platforms',
+  '.idea',
+  'test_results',
+  'package-lock.json',
+  'google-services.json'
+];
+
 function makeHeader(fileName) {
   return `//
 //  ${fileName}
@@ -35,6 +46,10 @@ function makeHeader(fileName) {
 
 function hasLicensedUnder(content) {
   return content.toLowerCase().includes('licensed under');
+}
+
+function shouldIgnore(filePath) {
+  return ignoredPatterns.some(pattern => filePath.includes(pattern));
 }
 
 function processFile(filePath) {
@@ -100,6 +115,9 @@ function walk(dir) {
   }
   fs.readdirSync(dir).forEach(file => {
     const fullPath = path.join(dir, file);
+    if (shouldIgnore(fullPath)) {
+      return;
+    }
     try {
       if (fs.statSync(fullPath).isDirectory()) {
         walk(fullPath);
